fd81c247a01214dd3dca603d11682d0163bb62db
kangooroux <bousquetalexandrepro@gmail.com>
2020-09-01 10:09:31 +0200

    Ajout de l'inscription

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegistrationController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/inscription", name="app_register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $form = $this->createForm(RegistrationFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user = $form->getData();
+            $password = $form->get('plainPassword')->getData();
+
+            // encode the plain password
+            $user
+                ->setPassword($passwordEncoder->encodePassword($user, $password))
+            ;
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+            // do anything else you need here, like send an email
+
+            $this->addFlash('success', 'Vous êtes désormais inscrit !');
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/index.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+}
