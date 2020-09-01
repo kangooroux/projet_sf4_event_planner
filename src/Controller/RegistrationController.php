e96e2a448a4ebe2ff961a9f7add72e5075695cb1
kangooroux <bousquetalexandrepro@gmail.com>
2020-09-01 10:20:51 +0200

    Ajout de la section profil pour changer ses paramètres

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -45,7 +45,7 @@ class RegistrationController extends AbstractController
             return $this->redirectToRoute('app_login');
         }
 
-        return $this->render('registration/index.html.twig', [
+        return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
     }
